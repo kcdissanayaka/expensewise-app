b8e984b9848317eb061980397e1bb6dee37d01da
Mujitha Manorathna <mujitha.m3@gmail.com>
2025-10-07 23:32:21 +0300

    Fix the error

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, SignUpScreen, ForgotPasswordScreen } from '../features/auth';
-import TabNavigator from '../features/dashboard/navigation/TabNavigator';
-import OnboardingNavigator from './OnboardingNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  LoginScreen,
+  SignUpScreen,
+  ForgotPasswordScreen,
+} from "../features/auth";
+import { SplashScreen, WelcomeCarouselScreen } from "../features/onboarding";
+import TabNavigator from "../features/dashboard/navigation/TabNavigator";
+import OnboardingNavigator from "./OnboardingNavigator";
 
 const Stack = createStackNavigator();
 
@@ -13,27 +18,26 @@ const AuthNavigator = () => {
       initialRouteName="Login"
       screenOptions={{
         headerShown: false,
-        cardStyle: { backgroundColor: '#FFFFFF' }
+        cardStyle: { backgroundColor: "#FFFFFF" },
       }}
     >
-      <Stack.Screen 
-        name="Login" 
+      <Stack.Screen
+        name="Login"
         component={LoginScreen}
-        options={{ title: 'Sign In' }}
+        options={{ title: "Sign In" }}
       />
-      <Stack.Screen 
-        name="SignUp" 
+      <Stack.Screen
+        name="SignUp"
         component={SignUpScreen}
-        options={{ title: 'Create Account' }}
+        options={{ title: "Create Account" }}
       />
-      <Stack.Screen 
-        name="ForgotPassword" 
+      <Stack.Screen
+        name="ForgotPassword"
         component={ForgotPasswordScreen}
-        options={{ title: 'Reset Password' }}
-         />
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="WelcomeCarousel" component={WelcomeCarouselScreen} />
-
+        options={{ title: "Reset Password" }}
+      />
+      <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Screen name="WelcomeCarousel" component={WelcomeCarouselScreen} />
     </Stack.Navigator>
   );
 };
