928783c0789a8e700cdf8c31b728316878ae0372
Mujitha Manorathna <mujitha.m3@gmail.com>
2025-10-08 00:27:12 +0300

    Fix error of import

--- src/navigation/OnboardingNavigator.js
+++ src/navigation/OnboardingNavigator.js
@@ -6,7 +6,7 @@ import SplashScreen from '../features/onboarding/screens/SplashScreen';
 import WelcomeCarouselScreen from '../features/onboarding/screens/WelcomeCarouselScreen';
 import GoalSettingScreen from '../features/onboarding/screens/GoalSettingScreen';
 import IncomeSetupScreen from '../features/onboarding/screens/IncomeSetupScreen';
-// import ExpenseSetupScreen from '../features/onboarding/screens/ExpenseSetupScreen';
+import ExpenseSetupScreen from '../features/onboarding/screens/ExpenseSetupScreen';
 // import AllocationSetupScreen from '../features/onboarding/screens/AllocationSetupScreen';
 // import PreferencesSetupScreen from '../features/onboarding/screens/PreferencesSetupScreen';
 // import OnboardingCompleteScreen from '../features/onboarding/screens/OnboardingCompleteScreen';
@@ -26,7 +26,8 @@ const OnboardingNavigator = () => {
       <Stack.Screen name="WelcomeCarousel" component={WelcomeCarouselScreen} />
       <Stack.Screen name="GoalSetting" component={GoalSettingScreen} />
       <Stack.Screen name="IncomeSetup" component={IncomeSetupScreen} />
-      {/* <Stack.Screen name="ExpenseSetup" component={ExpenseSetupScreen} />
+      <Stack.Screen name="ExpenseSetup" component={ExpenseSetupScreen} />
+      {/* 
       <Stack.Screen name="AllocationSetup" component={AllocationSetupScreen} />
       <Stack.Screen name="PreferencesSetup" component={PreferencesSetupScreen} />
       <Stack.Screen name="OnboardingComplete" component={OnboardingCompleteScreen} /> */}
